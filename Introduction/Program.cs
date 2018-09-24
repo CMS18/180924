139d43fb50e7b006e37b1f0a1f38a2c5ec87c54a
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-24 09:53:57 +0200

    Add project files.

---
+++ Introduction/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows";
+            //ShowLargeFilesWithoutLINQ(path);
+            ShowLargeFilesWithLINQ(path);
+        }
+
+        private static void ShowLargeFilesWithLINQ(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+
+            foreach (var file in query)
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLINQ(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparerOnLength());
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+
+        }
+    }
+
+    public class FileInfoComparerOnLength : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
